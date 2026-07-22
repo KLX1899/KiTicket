ef66c03c6f22ec17741f5c40b2e1c6e45a7063d1
BehradHZ <bhvsrt2006@gmail.com>
2026-07-22 21:21:50 +0330

    [fix] fix back-end directories

--- migrations/003_checkout_saga.down.sql
+++
@@ -1,3 +0,0 @@
-DROP INDEX IF EXISTS checkout.orders_recovery_idx;
-ALTER TABLE checkout.tickets DROP COLUMN IF EXISTS qr_payload;
-ALTER TABLE checkout.orders DROP COLUMN IF EXISTS booking_id, DROP COLUMN IF EXISTS reservation_fence;
